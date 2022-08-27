5c30b31812b889e360fd914635830c96a491db45
Yi Ching Wang <doo8409255@gmail.com>
2022-08-27 13:27:00 +0800

    UI

--- assets/script/main.js
+++ assets/script/main.js
@@ -247,6 +247,55 @@ $(function () {
   SetRatingStar();
 
 
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+      });
+
+      let portfolioFilters = select('.portfolio-flter', true);
+
+      on('click', '.portfolio-flter', function (e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function (el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+
+      }, true);
+
+      portfolioIsotope.arrange({
+        filter: '.filter-ext'
+      });
+    }
+
+  });
+
 
 
 });
